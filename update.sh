9cec6ca9485e590546f6aaf40e4309a0749c1afb
rachit-j <56803677+rachit-j@users.noreply.github.com>
2023-11-29 21:45:00 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 # WARNING: PLEASE DO NOT MODIFY THIS SCRIPT. IF YOU DO, YOU MUST RUN THE COMMANDS MANUALLY TO REPULL THE GIT REPOSITORY SO THAT THE SCRIPT DOES NOT CHANGE AS IT EXECUTES. MAKE SURE TO SEND AN ALERT IF THIS HAS CHANGED!
 
-echo "Updating and redeploying the ww3-backend. If no changes are shown, please run: service nginx restart"
+echo "Updating and redeploying the IGE Backend (Script made by yours truly). If no changes are shown, please run: service nginx restart"
 echo "----------------------------------------------------------------------------------------------------------------"
-cd /root/ww3-backend/
+cd /root/IGE-backend-v2/
 docker-compose down
 git pull .
 docker-compose up -d --build
